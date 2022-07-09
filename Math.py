c2185b02f3f7441570e28ea4a7bed4c3d61f90b3
tannerross22 <108552142+tannerross22@users.noreply.github.com>
2022-07-09 16:02:22 -0500

    Update Math.py

--- Math.py
+++ Math.py
@@ -3,41 +3,42 @@ import numpy as np
 
 atm = pd.read_csv("atmdata.csv")
 thrust = pd.read_csv("thrust.csv")
-Cd = pd.read_csv("RASaero.csv")
+Coefficient = pd.read_csv("RASaero.csv")
 
-
-def Thrust(time):
-
-    return thrust[thrust["Time"] <= time].iloc[-1]["Corrected Thrust"]
 
 
 def air_density(altitude):
     return atm[atm["Altitude[m]"] <= altitude].iloc[-1]["Density[kg/m3]"]
 
-def CD(mach):
-    return Cd[Cd["Mach"] <= mach].iloc[-1]["CD"]
 
+def Thrust(time):
+    return thrust[thrust["Time"] <= time].iloc[-1]["Corrected Thrust"]
+
+def CD(mach):
+    return Coefficient[Coefficient["Mach"] <= mach].iloc[-1]["CD"]
+
+def SOS(altitude):
+    return atm[atm["Altitude[m]"] <= altitude].iloc[-1]["Speed of sound[m/s]"]
+
+# Initial Condiitons
 time = 0
-inc = 0.25
+inc = 0.5
 wet = 120
 fuel = 60
 gravity = -9.81
 net_force = wet*gravity
-burn_time = 31
+burn_time = 19.43
 gradient = fuel/burn_time
 velocity = 0
 drag = 0
-# Pretty sure that this is wrong. 3.5" is 0.0889 m, so area should be closer to 0.025
-# area = 0.0081
 area = 0.025
 parea = 3.57
 pCd = 2.2
-altitude = 0
+altitude = 400
 density = air_density(altitude)
-mach = velocity/density
-D = CD(mach)
+Cd = 1.236
 avg = Thrust(time)
-
+sps = SOS(altitude)
 if net_force == 0:
     acceleration = gravity
 
@@ -64,36 +65,42 @@ alt = []
 vel = []
 net = []
 mac = []
+CDD = []
+ss = []
 
 while time < 10000:
     density = air_density(altitude)
     avg = Thrust(time)
+    sps = SOS(altitude)
+    mach = velocity/sps
+    Cd = CD(mach)
     thr.append(avg)
     tim.append(time)
     alt.append(altitude)
     vel.append(velocity)
     net.append(net_force)
     den.append(density)
-    mac.append(velocity/density)
+    CDD.append(Cd)
+    ss.append(sps)
+    mac.append(mach)
     if thrustactive(burn_time) is True:
         wet = wet - (gradient*inc)
-        drag = 1/2*area*D*density*velocity**2
+        drag = 1/2*area*Cd*density*velocity**2
         net_force = avg - wet*gravity - drag
         acceleration = net_force/wet
         velocity = velocity + acceleration*inc
         altitude = (velocity * inc + (1 / 2 * acceleration * inc ** 2)) + altitude
-        mach = velocity/density
         time += inc
-        # print(altitude, velocity, net_force)
+
     elif time >= burn_time and parachute(altitude) is False:
         avg = 0
-        drag = 1 / 2 * area * D * density * velocity ** 2
+        drag = 1 / 2 * area * Cd * density * velocity ** 2
         net_force = wet*gravity - drag
         acceleration = net_force / wet
         altitude = (velocity * inc + (1 / 2 * acceleration * inc ** 2)) + altitude
         velocity = velocity + (acceleration * inc)
-        # print(altitude, velocity, net_force)
         time += inc
+
     else:
         avg = 0
         drag = 1 / 2 * parea * pCd * density * (velocity ** 2)
@@ -101,7 +108,6 @@ while time < 10000:
         acceleration = net_force / wet
         altitude = (velocity * inc + (1 / 2 * acceleration * inc ** 2)) + altitude
         velocity = velocity + (acceleration * inc)
-        # print(altitude, velocity, net_force)
         time += inc
         if altitude <= 0:
             break
@@ -134,16 +140,21 @@ def show_thrust():
     plt.ylabel("Thrust (N)")
     plt.show()  
 
+def show_CD():
+    x = tim
+    y = CDD
+    plt.plot(x,y)
+    plt.show()
+
+
 def print_maxes():
     print("--- Maxes ---")
     print(f"Max Altitude: {max(alt):.0f} m")
+    print(f"Min Velocity: {min(vel):.0f} m/s")
     print(f"Max Velocity: {max(vel):.0f} m/s")
     print(f"Max Net Force: {max(net):.0f} N")
     print(f"Min Density: {min(den):.4f} kg/m^3")
-
+    print(f"Min CD: {min(CDD):.4f}")
+    print(f"Max mach: {max(mac):.4f}")
 
 print_maxes()
-# show_density()
-# show_thrust()
-show_altitude()
-show_density()
